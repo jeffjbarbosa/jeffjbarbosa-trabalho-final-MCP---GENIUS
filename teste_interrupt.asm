b01859424b37992829705b6fea8efa4ca1bd71df
Igor Godinho <godinho.igor@gmail.com>
2018-07-03 17:56:24 -0300

    teste interrupcao

--- teste_interrupt.asm
+++ teste_interrupt.asm
@@ -8,12 +8,22 @@ sw $a0, 8($k1)
 lw      $t0, 0xffff0000
 li      $t1, 0x00000000 # desabilita interrupcao keyboard
 
+li $v0, 1
+move $a0, $13
+syscall
+
+li $t3, 0x00000100
+and $t1, $12, $t3
+beq $t1, 0x0000100, if_keyboard_interrupt
+j end_if_keyboard_interrupt
+
+if_keyboard_interrupt:
 li $v0, 4
 la $a0, string
 syscall
-
 li $t2, 1
 sw $t2, flag
+end_if_keyboard_interrupt: nop
 
 lw $at, 0($k1)
 lw $v0, 4($k1)
@@ -26,28 +36,49 @@ mtc0 $k0, $14
 eret
 .kdata
 _regs: .space 12
-string: .asciiz "entrou na interrupcao!\n"
+string: .asciiz "\nentrou na interrupcao!\n"
 
-.ktext 0x80000180
+#----------------------------------------------------------------------------------------------------------------------------------------------
 .data
 texto: .asciiz "saiu da interrupcao."
 flag: .word 0
 .text
 .globl main
 main:
+
+# Enable interrupts in status register
+	mfc0    $t0, $12
+
+	# Disable all interrupt levels
+	li      $t1, 0x0000ff00		#EXC_INT_ALL_MASK
+	not     $t1, $t1
+	and     $t0, $t0, $t1
+	
+	# Enable console interrupt levels
+	li      $t1, 0x00000100 	#EXC_INT3_MASK KEYBOARD
+	or      $t0, $t0, $t1
+	
+	# Enable exceptions globally
+	li      $t1, 0x00000001		#EXC_ENABLE_MASK
+	or      $t0, $t0, $t1
+	
+	mtc0    $t0, $12
+
 key_wait:
     	    li $v0, 32
     	    li $a0, 3000
     	    syscall
-    	    
-	    lw      $t0, 0xffff0000
-	    li      $t1, 0x00000002 # habilita interrupcao
-	    sw	    $t1, 0($t0)   
+	   
+	   li      $t0, 0xffff0000     # Receiver control register
+	   li      $t1, 0x00000002     # Interrupt enable bit
+	   sw      $t1, ($t0)
 	    
 	    while:
 	    	lw $t0, flag
 	    	bnez $t0, endwhile
 	    j while
+	    
+	    
 	    endwhile: nop
 li $v0, 4
 la $a0, texto
