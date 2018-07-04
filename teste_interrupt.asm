3b7e86d937670e7dfca748d5cc7313582a00d03f
jeff.jbarbosa <jefferson.jaderbarbosa@gmail.com>
2018-07-03 23:06:32 -0300

    teste interrupt continue...

--- teste_interrupt.asm
+++ teste_interrupt.asm
@@ -4,6 +4,21 @@ la $k1, _regs
 sw $k0, 0($k1)
 sw $v0, 4($k1)
 sw $a0, 8($k1)
+<<<<<<< HEAD
+#li $v0, 1
+#mfc0 $a0, $13 # tem que ser mfc0 em vez de move
+#syscall
+li $t3, 0x00000100
+and $t1, $a0, $t3	# usar $12 na verdade se refere ao $t4, que é o registrador 12 do processador normal
+beq $t1, 0x0000100, if_keyboard_interrupt
+j end_if_keyboard_interrupt
+if_keyboard_interrupt:
+li $v0, 11
+lb $a0, 0xffff0004
+syscall
+#li $t2, 1
+#sw $t2, flag
+=======
 
 lw      $t0, 0xffff0000
 li      $t1, 0x00000000 # desabilita interrupcao keyboard
@@ -23,29 +38,36 @@ la $a0, string
 syscall
 li $t2, 1
 sw $t2, flag
+>>>>>>> b01859424b37992829705b6fea8efa4ca1bd71df
 end_if_keyboard_interrupt: nop
 
 lw $at, 0($k1)
 lw $v0, 4($k1)
 lw $a0, 8($k1)
 
-mfc0 $k0, $14
-addiu $k0, $k0, 4
-mtc0 $k0, $14
+# como foi gerado por interrupção, não soma +4 no PC
+#mfc0 $k0, $14
+#addiu $k0, $k0, 4
+#mtc0 $k0, $14
 
 eret
+
 .kdata
 _regs: .space 12
 string: .asciiz "\nentrou na interrupcao!\n"
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
 .data
-texto: .asciiz "saiu da interrupcao."
+enter: .asciiz "/n"
 flag: .word 0
+texto: .asciiz "saiu da interrupcao."
 .text
 .globl main
 main:
+<<<<<<< HEAD
+=======
 
+>>>>>>> b01859424b37992829705b6fea8efa4ca1bd71df
 # Enable interrupts in status register
 	mfc0    $t0, $12
 
@@ -73,9 +95,12 @@ key_wait:
 	   li      $t1, 0x00000002     # Interrupt enable bit
 	   sw      $t1, ($t0)
 	    
+	    lw $t0, enter
 	    while:
-	    	lw $t0, flag
-	    	bnez $t0, endwhile
+	    	#lw $t0, flag
+	    	#bnez $t0, endwhile
+	    	lw $t1, 0xffff0004
+	    	beq $t0, $t1, endwhile
 	    j while
 	    
 	    
